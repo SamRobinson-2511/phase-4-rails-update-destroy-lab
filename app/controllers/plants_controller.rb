1706d4b8572a97b2b150d4a9994890eb2c354c22
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2023-01-17 22:53:37 -0600

    commit tree 06dc937b43ff1221418bc59a04cfe6ac5047d097

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,7 +18,23 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = plant_find
+    plant.update!(plant_params)
+    render json: plant, status: 200
+  end
+
+  def destroy
+    plant = plant_find
+    plant.destroy
+    head :no_content
+  end
+
   private
+
+  def plant_find
+    Plant.find(params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
